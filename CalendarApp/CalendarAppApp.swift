3e354b020c2fa5dd91bcd975a2aac972903c29ac
koki <k.kusakabe1030@gmail.com>
2025-01-20 21:45:28 +0900

    Initial Commit

---
+++ CalendarApp/CalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppApp.swift
+//  CalendarApp
+//
+//  Created by Kusakabe Koki on 2025/01/20.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
